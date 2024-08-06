100b554f36239a436f9a0f747f696853cd7ae6c6
Hussain Ahmad <hussainahmad4658@gmail.com>
2024-08-06 14:45:25 +0500

    update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,34 +1,50 @@
 
 document.querySelectorAll('.drum')
     .forEach(e => {
-        let audio
-        switch (e.classList[0]) {
-            case 'w':
-                audio = new Audio('./sounds/crash.mp3')
-                break
-            case 'a':
-                audio = new Audio('./sounds/kick-bass.mp3')
-                break
-            case 's':
-                audio = new Audio('./sounds/snare.mp3')
-                break
-            case 'd':
-                audio = new Audio('./sounds/tom-1.mp3')
-                break
-            case 'j':
-                audio = new Audio('./sounds/tom-2.mp3')
-                break
-            case 'k':
-                audio = new Audio('./sounds/tom-3.mp3')
-                break
-            case 'l':
-                audio = new Audio('./sounds/tom-4.mp3')
-                break
-            default:
-                break
-        }
-
         e.addEventListener('click', () => {
-            audio.play()
+            playSound(e.classList[0])
         })
     })
+
+document.addEventListener('keypress', e => {
+    playSound(e.key)
+})
+
+function playSound(key){
+    let audio
+    switch (key) {
+        case 'w':
+            audio = new Audio('./sounds/crash.mp3')
+            break
+        case 'a':
+            audio = new Audio('./sounds/kick-bass.mp3')
+            break
+        case 's':
+            audio = new Audio('./sounds/snare.mp3')
+            break
+        case 'd':
+            audio = new Audio('./sounds/tom-1.mp3')
+            break
+        case 'j':
+            audio = new Audio('./sounds/tom-2.mp3')
+            break
+        case 'k':
+            audio = new Audio('./sounds/tom-3.mp3')
+            break
+        case 'l':
+            audio = new Audio('./sounds/tom-4.mp3')
+            break
+        default:
+            break
+    }
+    animateBtn(key)
+    audio.play()
+}
+
+function animateBtn(key){
+    const btn = document.querySelector('.' + key)
+    btn.classList.add('pressed')
+    setTimeout(() => {
+        btn.classList.remove('pressed')
+    }, 200);
+}
