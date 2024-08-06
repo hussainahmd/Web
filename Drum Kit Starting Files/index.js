3cee24f290bef9a4cfb2fdf5120a6b10162aed26
Hussain Ahmad <hussainahmad4658@gmail.com>
2024-08-06 13:46:01 +0500

    update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,3 +1,34 @@
-document.querySelectorAll('drum').forEach(addEventListener('click', function () {
-    alert('I got clicked')
-}))
+
+document.querySelectorAll('.drum')
+    .forEach(e => {
+        let audio
+        switch (e.classList[0]) {
+            case 'w':
+                audio = new Audio('./sounds/crash.mp3')
+                break
+            case 'a':
+                audio = new Audio('./sounds/kick-bass.mp3')
+                break
+            case 's':
+                audio = new Audio('./sounds/snare.mp3')
+                break
+            case 'd':
+                audio = new Audio('./sounds/tom-1.mp3')
+                break
+            case 'j':
+                audio = new Audio('./sounds/tom-2.mp3')
+                break
+            case 'k':
+                audio = new Audio('./sounds/tom-3.mp3')
+                break
+            case 'l':
+                audio = new Audio('./sounds/tom-4.mp3')
+                break
+            default:
+                break
+        }
+
+        e.addEventListener('click', () => {
+            audio.play()
+        })
+    })
