3e138e4809c22faaf91eac7a7ffc594591111028
Hussain Ahmad <hussainahmad4658@gmail.com>
2024-08-08 23:03:39 +0500

    u

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,22 +7,55 @@ let userClickedPattern = []
 let level = 0
 let started = false
 
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence()
+            }, 1000)
+        }
+    } else {
+        playSound("wrong")
+        $("body").addClass("game-over")
+        $("#level-title").text("Game Over, Press Any Key to Restart")
+
+        setTimeout(function () {
+            $("body").removeClass("game-over")
+        }, 200)
+
+        startOver()
+    }
+}
+
+function startOver() {
+    level = 0
+    gamePattern = []
+    started = false
+}
+
 $(document).keypress(e => {
     if (!started) {
-        $('h1').text('Level ' + level)
+        // $('h1').text('Level ' + level)
         nextSequence()
         started = true
     }
 })
 
 $('.btn').click(function () {
+    if(!started)
+        return
     let userChosenColor = this.id
     userClickedPattern.push(userChosenColor)
     playSound(userChosenColor)
     animatePress(userChosenColor)
+
+    checkAnswer(userClickedPattern.length - 1)
+    console.log('User:', userClickedPattern)
 })
 
 function nextSequence() {
+    userClickedPattern = []
     level++
     $('h1').text('Level ' + level)
     
@@ -32,6 +65,7 @@ function nextSequence() {
 
     $('#' + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
 
+    console.log('Game:',gamePattern)
     playSound(randomChosenColor)
 }
 
