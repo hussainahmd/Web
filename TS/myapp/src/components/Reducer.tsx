a0c13f385e5c0f3fad481877ef6fb938b7632a24
Hussain Ahmad <hussainahmad4658@gmail.com>
2024-08-19 01:08:33 +0500

    u

--- TS/myapp/src/components/Reducer.tsx
+++ TS/myapp/src/components/Reducer.tsx
@@ -1,21 +1,68 @@
-import { ReactNode, useState } from 'react'
+import { ChangeEvent, ReactNode, useReducer } from 'react'
 
 type ChildrenType = {
     children: (num: number) => ReactNode
 }
 
-const Reducer = ({ children }: ChildrenType) => {
-    const [count, setCount] = useState<number>(0)
+type State = {
+    count: number,
+    error: string | null
+    text?: string
+}
 
-    const increment = () => setCount(p => p + 1)
-    const decrement = () => setCount(p => p - 1)
+type Action = {
+    type: 'INCREMENT' | 'DECREMENT' | 'INPUT',
+    payload?: string
+}
+
+const reducer = (state: State, action: Action): State => {
+    const { type } = action
+    switch (type) {
+        case 'INCREMENT': {
+            const newCount = state.count + 1
+            const isError = newCount > 5
+            return {
+                ...state,
+                count: isError ? state.count : newCount,
+                error: isError ? 'Max reached' : null
+            }
+        }
+
+        case 'DECREMENT': {
+            const newCount = state.count - 1
+            const isError = newCount < 0
+            return {
+                ...state,
+                count: isError ? state.count : newCount,
+                error: isError ? 'Min reached' : null
+            }
+        }
+
+        case 'INPUT': {
+            return{...state,text: action.payload ?? ''}
+        }
+        default:
+            return state
+    }
+}
+
+const Reducer = ({ children }: ChildrenType) => {
+
+    const [state, dispatch] = useReducer<(state: State, action: Action) => State>(reducer, { count: 0, error: null })
+
+    const increment = () => dispatch({ type: 'INCREMENT' })
+    const decrement = () => dispatch({ type: 'DECREMENT' })
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => dispatch({type: 'INPUT', payload: e.target.value})
 
     return (
         <>
-            <h1>{children(count)}</h1>
-            <div>
+            <div className='container'>
+                <h1>{children(state.count)}</h1>
+                <h2>Error: {state.error}</h2>
                 <button onClick={increment}>+</button>
                 <button onClick={decrement}>-</button>
+                <input placeholder='Type something' onChange={handleInput} />
+                <h2>Text: {state.text}</h2>
             </div>
         </>
     )
